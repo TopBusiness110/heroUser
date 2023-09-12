e30ab1d97b21091184cb1935df84261de0748795
arabDevCom <semicolonsales@gmail.com>
2023-09-12 14:49:02 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+// import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'functions/functions.dart';
+import 'functions/notifications.dart';
+import 'pages/loadingPage/loadingpage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // FlutterError.onError = (errorDetails) {
+  //     FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  //   };
+  //   //Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  //   PlatformDispatcher.instance.onError = (error, stack) {
+  //     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+  //     return true;
+  //   };
+  checkInternetConnection();
+ initMessaging();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    platform = Theme.of(context).platform;
+    return GestureDetector(
+        onTap: () {
+          //remove keyboard on touching anywhere on the screen.
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+            FocusManager.instance.primaryFocus?.unfocus();
+          }
+        },
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'hero',
+            theme: ThemeData(),
+            home: const LoadingPage()));
+  }
+}
